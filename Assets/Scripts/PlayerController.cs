a5ce2eb1b993a7e8c66a9f3f99916f945838cf7a
daehoon0409 <wwwbjacau85@gmail.com>
2024-09-25 11:02:54 +0900

    [Feat] 1인칭 3인칭 카메라 제작

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float movoSpeed = 5.0f;    //이동 속도
+    public float jumpForce = 5.0f;    //점프 힘
+
+    [Header("Camera Settings")]
+    public Camera firstPersonCamera;
+    public Camera thirdPersonCamera;
+
+    public float radius = 5.0f;
+    public float minRadius = 1.0f;
+    public float maxRadius = 10.0f;
+
+    public float yMinLimit = 30;
+    public float yMaxLimit = 90;
+
+    private float theta = 0.0f;
+    private float phi = 0.0f;
+    private float targetVerticalRoataion = 0;
+    private float verticalRotationSpeed = 240f;
+
+    public float mouseSenesitivity = 2f;
+
+    private bool isFirstPerson = true;
+    private bool isGrounded;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        SetupCameras();
+    }
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleJump();
+        HandleCameraToggle();
+    }
+
+    void SetActiveCanera()
+    {
+        firstPersonCamera.gameObject.SetActive(isFirstPerson);
+        thirdPersonCamera.gameObject.SetActive(!isFirstPerson);
+    }
+    void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSenesitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSenesitivity;
+
+        theta += mouseX;
+        theta = Mathf.Repeat(theta, 360f);
+
+        targetVerticalRoataion -= mouseY;
+        targetVerticalRoataion = Mathf.Clamp(targetVerticalRoataion, yMinLimit, yMaxLimit);
+        phi = Mathf.MoveTowards(phi, targetVerticalRoataion, verticalRotationSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0.0f, theta, 0.0f);
+
+        if(isFirstPerson)
+        {
+          firstPersonCamera.transform.localRotation = Quaternion.Euler(phi, 0.0f, 0.0f);
+        }
+        else
+        {
+            float x = radius * Mathf.Sin(Mathf.Deg2Rad * phi) * Mathf.Cos(Mathf.Deg2Rad * theta);
+            float y = radius * Mathf.Cos(Mathf.Deg2Rad * phi);
+            float z = radius * Mathf.Sin(Mathf.Deg2Rad * phi) * Mathf.Sin(Mathf.Deg2Rad * theta);
+
+            thirdPersonCamera.transform.position = transform.position + new Vector3(x, y, z);
+            thirdPersonCamera.transform.LookAt(transform);
+
+            radius = Mathf.Clamp(radius - Input.GetAxis("Mouse ScrolWheel") * 5, minRadius, maxRadius);
+        }
+    }
+
+    void HandleCameraToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isFirstPerson = !isFirstPerson;
+            SetActiveCanera();
+        }
+    }
+    void SetupCameras()
+    {
+        firstPersonCamera.transform.localPosition = new Vector3(0f, 0.6f, 0f);
+        firstPersonCamera.transform.localRotation = Quaternion.identity;
+    }
+    //플레이어 점프를 처리하는 함수
+    void HandleJump()
+    {
+        //점프 버튼을 누르고 땅에 있을 때
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void HandleMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerical = Input.GetAxis("Vertical");
+
+        if (!isFirstPerson)
+        {
+            Vector3 cameraForward = thirdPersonCamera.transform.forward;
+            cameraForward.y = 0f;
+            cameraForward.Normalize();
+
+            Vector3 cameraRight = thirdPersonCamera.transform.right;
+            cameraRight.y = 0f;
+            cameraRight.Normalize();
+
+            Vector3 movement = cameraForward * moveVerical + cameraRight * moveHorizontal;
+            rb.MovePosition(rb.position + movement * movoSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVerical;
+            rb.MovePosition(rb.position + movement * movoSpeed * Time.deltaTime);
+        }
+    }
+    //플레이어가 땅에 닿아 있는지 감지
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
